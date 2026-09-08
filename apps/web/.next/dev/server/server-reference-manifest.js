616271984dac1b8c2b0f04a1952b1588f2c5ae84
Enoque Macanda <enoque.macanda@gmail.com>
2026-09-08 11:31:00 +0200

    chore: remove generated build artifacts

--- apps/web/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"B5dn5vqNJZoHvH1UCRqeabbPoVCTxD5SZPi2RSeh/Cw=\"\n}"
--- apps/web/.next/server/chunks/ssr/00r__next_dist_client_components_builtin_global-error_0ilwyxo.js
+++
@@ -1,3 +0,0 @@
-module.exports=[72048,(a,b,c)=>{let{createClientModuleProxy:d}=a.r(74262);a.n(d("[project]/node_modules/.pnpm/next@16.2.11_@babel+core@7.29.7_react-dom@19.2.4_react@19.2.4__react@19.2.4/node_modules/next/dist/client/components/builtin/global-error.js <module evaluation>"))},91537,(a,b,c)=>{let{createClientModuleProxy:d}=a.r(74262);a.n(d("[project]/node_modules/.pnpm/next@16.2.11_@babel+core@7.29.7_react-dom@19.2.4_react@19.2.4__react@19.2.4/node_modules/next/dist/client/components/builtin/global-error.js"))},2561,a=>{"use strict";a.i(72048);var b=a.i(91537);a.n(b)},82281,a=>{a.n(a.i(2561))}];
-
-//# sourceMappingURL=00r__next_dist_client_components_builtin_global-error_0ilwyxo.js.map
