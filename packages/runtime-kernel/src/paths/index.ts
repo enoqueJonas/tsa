c2c3f23f78141dbdad2d4a746a7fa5378f54c453
Enoque Macanda <enoque.macanda@gmail.com>
2026-09-09 10:38:13 +0200

    feat: export Security Steward curriculum

--- packages/runtime-kernel/src/paths/index.ts
+++ packages/runtime-kernel/src/paths/index.ts
@@ -12,6 +12,7 @@ export * from "./platform-builder";
 export * from "./delivery-engineer";
 export * from "./cloud-engineer";
 export * from "./quality-steward";
+export * from "./security-steward";
 export * from "./internal-dependency-management";
 export * from "./thinking-like-an-engineer";
 export * from "./systems-thinking";
